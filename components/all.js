978276fe27fa8cc866ac4c58439d1d6a520d3dfb
Cao Quý Đăng <37436780+wowKimsohyun@users.noreply.github.com>
2020-05-17 10:12:12 +0700

    js for Cart(open/close)

--- components/all.js
+++ components/all.js
@@ -2,19 +2,19 @@
 /*---------------------------Handle onClick Shop sub-menu------------------------*/
 /*-------------------------------------------------------------------------------*/
 
-// Click drop down
-document.addEventListener("DOMContentLoaded", function () {
 
+document.addEventListener("DOMContentLoaded", function () {
+    
+    // Click drop down
     var button = document.getElementById('tle'),
         drop = document.getElementById('drop-down');
-    console.log(button);
-    button.onclick = function () {
+        console.log(button);
+        button.onclick = function () {
         drop.classList.toggle('show');
     }
-}, false);
 
-// Close drop down
-window.onclick = function (event) {
+    // Close drop down
+    window.onclick = function (event) {
     if (!event.target.matches('.dropbtn')) {
         var dropdowns = document.getElementsByClassName("menu-layer2");
         var i;
@@ -26,3 +26,33 @@ window.onclick = function (event) {
         }
     }
 }
+}, false);
+
+
+
+/*-------------------------------------------------------------------------------*/
+/*---------------------------Cart event open/close-------------------------------*/
+/*-------------------------------------------------------------------------------*/
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    var black = document.querySelector('.black-screen'),
+        cart = document.querySelector('.content-cart'),
+        button = document.querySelector('.cart'),
+        overflow = document.querySelector('body'),
+        close = document.querySelector('.close');
+
+    // Open
+    button.onclick = function () {
+        black.classList.add('show');
+        cart.classList.add('move-to-left');
+        overflow.classList.add('remove-scroll');
+    }
+
+    // Close
+    close.onclick = function () {
+        black.classList.remove('show');
+        cart.classList.remove('move-to-left');
+        overflow.classList.remove('remove-scroll');
+    }
+}, false);
